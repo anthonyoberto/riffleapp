6589e713349be5a125b2189809db6d44b4b39b36
Jamie Emerson <ingamemerson@gmail.com>
2014-03-17 13:42:15 -0600

    updated user and sound schemas

--- api/server/models/schema.js
+++ api/server/models/schema.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 var ObjectId = mongoose.Schema.Types.ObjectId;
+var Schema = mongoose.Schema;
+
 var userSchema = mongoose.Schema({
   id: Objectid,
   name: String,
@@ -15,4 +17,21 @@ var userSchema = mongoose.Schema({
 
 });
 
+var soundSchema = new Schema({
+  id: Objectid,
+  tags: Array,
+  length: number,
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  modified: {
+    type: Date,
+    defualt: Date
+  },
+  source: String
+
+});
+
 module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Sound', soundSchema);
--- api/server/models/soundsegment.js
+++ api/server/models/soundsegment.js
@@ -1,24 +1,19 @@
 var mongoose = require('mongoose');
-var soundSchema = mongoose.Schema({
-  id: {
-
-  },
-  tags: {
-
-  },
-  length: {
-
-  },
+var ObjectId = mongoose.Schema.Types.ObjectId;
+var Schema = mongoose.Schema;
+var soundSchema = new Schema({
+  id: Objectid,
+  tags: Array,
+  length: number,
   created: {
     type: Date,
     default: Date.now
   },
   modified: {
-
+    type: Date,
+    defualt: Date
   },
-  source: {
-
-  }
+  source: String
 
 });
 
